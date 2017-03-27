70f125ec484aae8cf7c90dba573dc7427e174e46
Cameron Taggart <cameron@tachyus.com>
2017-03-26 22:45:52 -0500

    fsharp library test

---
+++ src/ClassLibrary1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public static class Class1
+    {
+        public static int TestXy(int value)
+        {
+            if (value == 0)
+            {
+                throw new Exception("Not valid");
+            }
+            return value;
+        }
+    }
+}
