772ba5c4407e98279c9b0930307cdf390e87964b
Cameron Taggart <cameron.taggart@gmail.com>
2018-01-10 10:19:25 -0500

    try out source linked Newtonsoft.Json

---
+++ TryJson/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TryJson
+{
+    // https://www.newtonsoft.com/json/help/html/SerializeObject.htm
+
+    class Account
+    {
+        public string Email { get; set; }
+        public bool Active { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new Account
+            {
+                Email = "james@example.com",
+                Active = true
+            };
+
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(account);
+            Console.WriteLine(json);
+        }
+    }
+}
